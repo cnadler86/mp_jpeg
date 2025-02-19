1625cb804de819837f41f54de34693700aa34eed
Christopher Nadler <147471517+cnadler86@users.noreply.github.com>
2025-02-19 10:09:57 +0100

    Update micropython.cmake

--- src/micropython.cmake
+++ src/micropython.cmake
@@ -1,5 +1,5 @@
 # Register the component
-idf_component_register(SRCS "jpeg_esp.c"
+idf_component_register(SRCS "src/jpeg_esp.c"
                        INCLUDE_DIRS "."
                        REQUIRES esp_new_jpeg)
 
@@ -7,11 +7,11 @@ idf_component_register(SRCS "jpeg_esp.c"
 add_library(usermod_mp_jpeg INTERFACE)
 
 target_sources(usermod_mp_jpeg INTERFACE
-    ${CMAKE_CURRENT_LIST_DIR}/jpeg_esp.c
+    ${CMAKE_CURRENT_LIST_DIR}/src/jpeg_esp.c
 )
 
 target_include_directories(usermod_mp_jpeg INTERFACE
-    ${CMAKE_CURRENT_LIST_DIR}
+    ${CMAKE_CURRENT_LIST_DIR/src}
 )
 
 # Link the usermod library with usermod_mp_jpeg
